0eb78af45b2b6e977158359134e685ca22543fd1
Subhoshri <subhoshripal@gmail.com>
2026-04-07 12:06:14 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.jsx</code>
           </p>
         </div>
         <button
